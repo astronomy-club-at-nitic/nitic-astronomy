67056b1257378df565c803c31cbf65e82e3bf81b
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-04-18 23:42:17 +0900

    refactor: ♻️ (`/`) 共通化して不要になったメタデータの設定と一部マークアップを削除した。

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,18 +1,9 @@
-import type { Metadata } from 'next';
 import type { FC } from 'react';
 
 const RootPage: FC = () => (
   <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-keyplate-12">
-    <hgroup className="text-center">
-      <h1 className="my-4 text-4xl font-bold">
-        <span className="rounded-full bg-primary-3 px-8 py-2 text-primary-11">Astronomy Club</span> at&nbsp;NITIC
-      </h1>
-      <p>
-        Next 13 with <code className="bg-keyplate-3 text-keyplate-11">/app</code>{' '}
-        <span className="font-bold text-teal-11">styled using Tailwind CSS</span>
-      </p>
-    </hgroup>
-    <article className="flex max-w-2xl flex-col gap-4">
+    <article className="flex max-w-2xl flex-col gap-4 p-6">
+      <h1 className="my-6 font-main text-5xl font-bold text-keyplate-12">天文部へようこそ！</h1>
       <section className="font-main">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -42,26 +33,3 @@ const RootPage: FC = () => (
 );
 
 export default RootPage;
-
-export const generateMetadata = async (): Promise<Metadata> => {
-  const metadata: Metadata = {
-    title: '茨城高専 天文部',
-    description: 'まったり星を眺めるのも、己の興味に従って研究をするのも、天文部ではすべてが自由。みなさんの入部をお待ちしています！',
-    viewport: {
-      width: 'device-width',
-      initialScale: 1.0,
-    },
-    icons: '/favicon.ico',
-    openGraph: {
-      type: 'website',
-      url: `https://${process.env['NEXT_PUBLIC_HOSTNAME']}`,
-      images: `https://${process.env['NEXT_PUBLIC_HOSTNAME']}/ogp.png`,
-    },
-    twitter: {
-      card: 'summary_large_image',
-      site: '@NITIC_astronomy',
-    },
-  };
-
-  return metadata;
-};
