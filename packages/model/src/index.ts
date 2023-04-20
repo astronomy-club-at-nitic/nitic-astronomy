0ef2191dcf1f1435e1ead102b20fd46a26791c0d
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-04-20 22:12:00 +0900

    refactor: ♻️ (model) エントリファイル`index.ts`を`src`直下に移した。

---
+++ packages/model/src/index.ts
@@ -0,0 +1,11 @@
+export type { Article, ArticleDetail } from '~model/model/article.model';
+export type { Member } from '~model/model/member.model';
+export type { Tag } from '~model/model/tag.model';
+export type { Image } from '~model/model/image.model';
+export type { Id } from '~model/model/id.model';
+
+export { ArticleSchema, ArticleDetailSchema } from '~model/model/article.model';
+export { MemberSchema } from '~model/model/member.model';
+export { TagSchema } from '~model/model/tag.model';
+export { ImageSchema } from '~model/model/image.model';
+export { IdSchema } from '~model/model/id.model';
