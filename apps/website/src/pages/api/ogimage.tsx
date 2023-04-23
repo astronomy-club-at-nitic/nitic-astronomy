98008c04acfb7b7cab3226ef80ab38b61e8987cb
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-04-23 14:29:48 +0900

    wip: 🚧 (static)

--- apps/website/src/pages/api/ogimage.tsx
+++ apps/website/src/pages/api/ogimage.tsx
@@ -4,27 +4,27 @@
 
 import { ImageResponse } from '@vercel/og';
 
-type OgProps = {
-  title: string;
-  cover?: string;
-  authoricon?: string;
-  authorname?: string;
-  authorrole?: string;
-  authorcount?: number;
-};
+// type OgProps = {
+//   title: string;
+//   cover?: string;
+//   authoricon?: string;
+//   authorname?: string;
+//   authorrole?: string;
+//   authorcount?: number;
+// };
 
 // Local custom fonts: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-custom-font
-const domain = new URL(process.env['VERCEL_URL'] ? `https://${process.env['VERCEL_URL']}` : `http://localhost:${process.env['PORT'] || 3000}`);
+// const domain = new URL(process.env['VERCEL_URL'] ? `https://${process.env['VERCEL_URL']}` : `http://localhost:${process.env['PORT'] || 3000}`);
 // const getFont = fetch(new URL('../../../core/font/NotoSansJP/NotoSansJP-Bold.woff', import.meta.url)).then((res) => res.arrayBuffer());
 
 // Local static images: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-local-image
-// const getOgTemplateImage = fetch(new URL('../../../public/og/og-template.png', import.meta.url)).then((res) => res.arrayBuffer());
+const getOgTemplateImage = fetch(new URL('../../../public/og/og-template.png', import.meta.url)).then((res) => res.arrayBuffer());
 // const getCoverPlaceholderImage = fetch(new URL('../../../public/og/cover-placeholder.jpg', import.meta.url)).then((res) => res.arrayBuffer());
 // const getAuthorIconPlaceholderImage = fetch(new URL('../../../public/telescope.png', import.meta.url)).then((res) => res.arrayBuffer());
 // Avoiding using cache for now since the size of edge functions is limited to 1MB
-const getOgTemplateImage = Promise.resolve(new URL('/og/og-template.png', domain).href);
-const getCoverPlaceholderImage = Promise.resolve(new URL('/og/cover-placeholder.jpg', domain).href);
-const getAuthorIconPlaceholderImage = Promise.resolve(new URL('/telescope.png', domain).href);
+// const getOgTemplateImage = Promise.resolve(new URL('/og/og-template.png', domain).href);
+// const getCoverPlaceholderImage = Promise.resolve(new URL('/og/cover-placeholder.jpg', domain).href);
+// const getAuthorIconPlaceholderImage = Promise.resolve(new URL('/telescope.png', domain).href);
 
 // Segment configs:
 export const config = {
@@ -36,30 +36,30 @@ export const config = {
 export default async function handler(request: Request) {
   try {
     // Parse parameters
-    const { searchParams } = new URL(request.url);
-    const paramObject = Object.fromEntries(searchParams.entries());
-    const props: OgProps = {
-      title: paramObject['title'] || '茨城高専 天文部へようこそ',
-      cover: paramObject['cover'],
-      authoricon: paramObject['authoricon'],
-      authorname: paramObject['authorname'],
-      authorrole: paramObject['authorrole'],
-      authorcount: paramObject['authorcount'] ? Number(paramObject['authorcount']) : undefined,
-    };
+    // const { searchParams } = new URL(request.url);
+    // const paramObject = Object.fromEntries(searchParams.entries());
+    // const props: OgProps = {
+    //   title: paramObject['title'] || '茨城高専 天文部へようこそ',
+    //   cover: paramObject['cover'],
+    //   authoricon: paramObject['authoricon'],
+    //   authorname: paramObject['authorname'],
+    //   authorrole: paramObject['authorrole'],
+    //   authorcount: paramObject['authorcount'] ? Number(paramObject['authorcount']) : undefined,
+    // };
 
     // Load assets (will be cached)
     const ogTemplateImage = await getOgTemplateImage;
-    const coverPlaceholderImage = await getCoverPlaceholderImage;
-    const authorIconPlaceholderImage = await getAuthorIconPlaceholderImage;
+    // const coverPlaceholderImage = await getCoverPlaceholderImage;
+    // const authorIconPlaceholderImage = await getAuthorIconPlaceholderImage;
     // const font = await getFont;
 
     // Define aliases
-    const title = props.title.length > 32 ? `${props.title.slice(0, 31)}…` : props.title;
-    const cover = props.cover || coverPlaceholderImage;
-    const authoricon = props.authoricon || authorIconPlaceholderImage;
-    const { authorname, authorrole, authorcount } = props;
-    const shouldRenderAuthor = !!authorname;
-    const shouldRenderAuthorCount = shouldRenderAuthor && !!authorcount && authorcount > 1;
+    // const title = props.title.length > 32 ? `${props.title.slice(0, 31)}…` : props.title;
+    // const cover = props.cover || coverPlaceholderImage;
+    // const authoricon = props.authoricon || authorIconPlaceholderImage;
+    // const { authorname, authorrole, authorcount } = props;
+    // const shouldRenderAuthor = !!authorname;
+    // const shouldRenderAuthorCount = shouldRenderAuthor && !!authorcount && authorcount > 1;
 
     // Render an open graph image using Satori
     return new ImageResponse(
@@ -106,7 +106,9 @@ export default async function handler(request: Request) {
               height={525}
               // `img.src` accepts ArrayBuffer as well as string
               // Refer: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-local-image
-              src={cover as unknown as string}
+              src={
+                'https://images.unsplash.com/photo-1536893827774-411e1dc7c902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+              }
               style={{
                 position: 'absolute',
                 inset: 0,
@@ -134,7 +136,9 @@ export default async function handler(request: Request) {
               height={525}
               // `img.src` accepts ArrayBuffer as well as string
               // Refer: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-local-image
-              src={cover as unknown as string}
+              src={
+                'https://images.unsplash.com/photo-1536893827774-411e1dc7c902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+              }
               style={{
                 boxShadow: '3.56672px 3.56672px 57.0675px rgba(77, 27, 117, 0.1), inset 3.56672px 3.56672px 28.5338px rgba(255, 255, 255, 0.25)',
               }}
@@ -148,71 +152,72 @@ export default async function handler(request: Request) {
               top: 240,
               left: 76,
               width: 600,
-              fontSize: title.length > 18 ? 48 : 64,
+              fontSize: 64,
               lineHeight: '120%',
               letterSpacing: '0.05em',
               textOverflow: 'ellipsis',
               lineClamp: 2,
             }}
           >
-            {title}
+            こんにちは世界！
           </p>
-          {shouldRenderAuthor && (
+          <div
+            style={{
+              position: 'absolute',
+              display: 'flex',
+              gap: 24,
+              top: 472,
+              left: 76,
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+            }}
+          >
+            <img
+              width={96}
+              height={96}
+              src={'https://avatars.githubusercontent.com/u/16751535?v=4'}
+              style={{ borderRadius: '50%', border: '4px solid #F1F3F5' }}
+            />
             <div
               style={{
-                position: 'absolute',
                 display: 'flex',
-                gap: 24,
-                top: 472,
-                left: 76,
-                flexDirection: 'row',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
               }}
             >
-              <img width={96} height={96} src={authoricon as unknown as string} style={{ borderRadius: '50%', border: '4px solid #F1F3F5' }} />
-              <div
+              <p
                 style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'flex-start',
+                  margin: 0,
+                  fontSize: 32,
                 }}
               >
-                <p
-                  style={{
-                    margin: 0,
-                    fontSize: 32,
-                  }}
-                >
-                  {authorname}
-                </p>
-                <p
-                  style={{
-                    margin: 0,
-                    fontSize: 24,
-                    color: '#687076',
-                  }}
-                >
-                  {authorrole}
-                </p>
-              </div>
-              {shouldRenderAuthorCount && (
-                <p
-                  style={{
-                    margin: 0,
-                    padding: '24px 16px',
-                    background: '#ECEEF0',
-                    borderRadius: 999,
-                    fontSize: 24,
-                    color: '#687076',
-                  }}
-                >
-                  +{authorcount}名
-                </p>
-              )}
+                ReoHakase
+              </p>
+              <p
+                style={{
+                  margin: 0,
+                  fontSize: 24,
+                  color: '#687076',
+                }}
+              >
+                テスト
+              </p>
             </div>
-          )}
+            <p
+              style={{
+                margin: 0,
+                padding: '24px 16px',
+                background: '#ECEEF0',
+                borderRadius: 999,
+                fontSize: 24,
+                color: '#687076',
+              }}
+            >
+              +123名
+            </p>
+          </div>
         </div>
       ),
       {
