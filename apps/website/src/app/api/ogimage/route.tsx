445224796da6782b564b89b6d7e6a4c3b35bb092
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-04-23 13:19:05 +0900

    fix: 🐛 (`/api/ogimage`) アセットのURL解決部分を`new URL()`を使ったものに修正した。

--- apps/website/src/app/api/ogimage/route.tsx
+++ apps/website/src/app/api/ogimage/route.tsx
@@ -22,9 +22,9 @@ const domain = new URL(process.env['VERCEL_URL'] ? `https://${process.env['VERCE
 // const getCoverPlaceholderImage = fetch(new URL('../../../../public/og/cover-placeholder.jpg', import.meta.url)).then((res) => res.arrayBuffer());
 // const getAuthorIconPlaceholderImage = fetch(new URL('../../../../public/telescope.png', import.meta.url)).then((res) => res.arrayBuffer());
 // Avoiding using cache for now since the size of edge functions is limited to 1MB
-const getOgTemplateImage = Promise.resolve(domain + '/og/og-template.png');
-const getCoverPlaceholderImage = Promise.resolve(domain + '/og/cover-placeholder.jpg');
-const getAuthorIconPlaceholderImage = Promise.resolve(domain + '/telescope.png');
+const getOgTemplateImage = Promise.resolve(new URL('/og/og-template.png', domain));
+const getCoverPlaceholderImage = Promise.resolve(new URL('/og/cover-placeholder.jpg', domain));
+const getAuthorIconPlaceholderImage = Promise.resolve(new URL('/telescope.png', domain));
 
 // Segment configs: https://beta.nextjs.org/docs/api-reference/segment-config
 export const runtime = 'edge';
