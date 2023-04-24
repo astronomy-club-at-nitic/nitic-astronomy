6d638b07e6c5b4990d01d4d99c0012f57ac20a52
shio <85730998+dino3616@users.noreply.github.com>
2023-04-24 11:26:53 +0900

    refactor: ♻️ (root/footer): next/link に明示的に URL Object を渡すように修正

--- apps/website/src/module/root/ui/layout/footer/footer.presenter.tsx
+++ apps/website/src/module/root/ui/layout/footer/footer.presenter.tsx
@@ -8,35 +8,50 @@ type FooterProps = ComponentPropsWithoutRef<'footer'> & {
 };
 
 export const Footer: FC<FooterProps> = ({ isSm, ...props }) => (
-  <footer className="flex justify-center bg-dot-pattern p-6" {...props}>
+  <footer className="flex justify-center bg-dot-pattern-light p-6" {...props}>
     <div className="flex flex-col gap-2">
       <div className="flex flex-col gap-2 border-b-2 border-slate-light-6 pb-2 sm:flex-row sm:gap-6">
         <div className="flex flex-col gap-2 border-b-2 border-slate-light-6 pb-2 sm:border-b-0 sm:border-r-2 sm:pr-6">
           <div className="flex gap-4">
-            <Image src="/image/logo/dark.svg" sizes={64} alt="logo dark" className="h-16 w-16" />
-            <Image src="/image/logo/light.svg" sizes={64} alt="logo light" className="h-16 w-16" />
+            <Image src="/image/logo/dark.svg" sizes={64} alt="天文部のロゴ" className="h-16 w-16" />
+            <Image src="/image/logo/light.svg" sizes={64} alt="天文部のロゴ" className="h-16 w-16" />
           </div>
-          <Image src="/image/logo/nitic.png" width={isSm ? 274 : 342} height={isSm ? 44 : 55} alt="logo nitic" />
+          <Link href={new URL('https://www.ibaraki-ct.ac.jp/')} target="_blank" rel="noopener noreferrer">
+            <Image src="/image/logo/nitic.png" width={isSm ? 274 : 342} height={isSm ? 44 : 55} alt="茨城工業高等専門学校のロゴ" />
+          </Link>
         </div>
         <ul className="flex flex-col gap-3 py-3 font-bold text-slate-light-11">
           <li className="flex items-center gap-1">
             <RxTwitterLogo />
-            Twitter <Link href={new URL('https://twitter.com/nitic_astronomy')}>@nitic_astronomy</Link>
+            Twitter
+            <Link href={new URL('https://twitter.com/nitic_astronomy/')} target="_blank" rel="noopener noreferrer">
+              @nitic_astronomy
+            </Link>
           </li>
           <li className="flex items-center gap-1">
             <RxInstagramLogo />
-            Instagram <Link href={new URL('https://www.instagram.com/nitic_astronomy/')}>@nitic_astronomy</Link>
+            Instagram
+            <Link href={new URL('https://www.instagram.com/nitic_astronomy/')} target="_blank" rel="noopener noreferrer">
+              @nitic_astronomy
+            </Link>
           </li>
           <li className="flex items-center gap-1">
             <RxNotionLogo />
             Notionで
-            <Link href={new URL('https://nitic-astronomy.notion.site/Astronomy-Club-at-NITIC-a2ada4c005d647dd866767a69570e1a2')}>
+            <Link
+              href={new URL('https://nitic-astronomy.notion.site/Astronomy-Club-at-NITIC-a2ada4c005d647dd866767a69570e1a2/')}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               天文部の概要を知る
             </Link>
           </li>
           <li className="flex items-center gap-1">
             <RxGithubLogo />
-            GitHubで<Link href={new URL('https://github.com/astronomy-club-at-nitic')}>天文部の取り組みを見る</Link>
+            GitHubで
+            <Link href={new URL('https://github.com/astronomy-club-at-nitic/')} target="_blank" rel="noopener noreferrer">
+              天文部の取り組みを見る
+            </Link>
           </li>
         </ul>
       </div>
