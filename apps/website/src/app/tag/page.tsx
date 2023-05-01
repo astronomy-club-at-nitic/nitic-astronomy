9f8ad54749201c8c8a7efc40836cc01b23d0477d
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-01 22:37:46 +0900

    feat: ⚰️ (`/article` `/member` `/member`) 不要なLorem Ipsumを削除した。

--- apps/website/src/app/tag/page.tsx
+++ apps/website/src/app/tag/page.tsx
@@ -20,30 +20,6 @@ const TagsPage = async (): Promise<ReactElement> => {
         <pre className="w-full whitespace-pre-wrap break-all bg-keyplate-3 p-6 font-mono text-sm text-keyplate-11">
           <code>{JSON.stringify(tags, null, 2)}</code>
         </pre>
-        <section className="font-main">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            そこでは、あらゆる事が可能である。人は一瞬にして氷雲の上に飛躍し大循環の風を従へて北に旅する事もあれば、赤い花杯の下を行く蟻と語ることもできる。
-            罪や、かなしみでさへそこでは聖くきれいにかゞやいてゐる。
-          </p>
-        </section>
-        <section className="font-article text-article">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            そこでは、あらゆる事が可能である。人は一瞬にして氷雲の上に飛躍し大循環の風を従へて北に旅する事もあれば、赤い花杯の下を行く蟻と語ることもできる。
-            罪や、かなしみでさへそこでは聖くきれいにかゞやいてゐる。
-          </p>
-        </section>
       </article>
     </div>
   );
