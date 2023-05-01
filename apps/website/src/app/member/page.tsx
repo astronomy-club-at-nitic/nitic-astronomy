550de591ef01de8b97f207d44c461bb5069c5681
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-01 21:54:54 +0900

    feat: ✨ (`/member`) 部員紹介ページの静的パス生成と内容の表示(仮)を実装した。

--- apps/website/src/app/member/page.tsx
+++ apps/website/src/app/member/page.tsx
@@ -1,37 +1,54 @@
+import { getMembers, getArticleDetail } from '@nitic-astronomy/cms';
 import type { Metadata } from 'next';
-import type { FC } from 'react';
+import type { ReactElement } from 'react';
 
-const MembersPage: FC = () => (
-  <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-keyplate-12">
-    <article className="flex max-w-2xl flex-col gap-4 p-6">
-      <h1 className="my-6 font-main text-5xl font-bold text-keyplate-12">部員紹介</h1>
-      <section className="font-main">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-          deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          そこでは、あらゆる事が可能である。人は一瞬にして氷雲の上に飛躍し大循環の風を従へて北に旅する事もあれば、赤い花杯の下を行く蟻と語ることもできる。
-          罪や、かなしみでさへそこでは聖くきれいにかゞやいてゐる。
-        </p>
-      </section>
-      <section className="font-article text-article">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-          deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          そこでは、あらゆる事が可能である。人は一瞬にして氷雲の上に飛躍し大循環の風を従へて北に旅する事もあれば、赤い花杯の下を行く蟻と語ることもできる。
-          罪や、かなしみでさへそこでは聖くきれいにかゞやいてゐる。
-        </p>
-      </section>
-    </article>
-  </div>
-);
+const MembersPage = async (): Promise<ReactElement> => {
+  const article = await getArticleDetail({
+    id: 'member-introduction',
+  });
+  const members = await getMembers();
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-keyplate-12">
+      <article className="flex max-w-2xl flex-col gap-4 p-6">
+        <h1 className="my-6 font-main text-5xl font-bold text-keyplate-12">部員紹介</h1>
+        <section className="font-main">
+          <h2 className="text-heading2 font-bold">紹介記事</h2>
+          <pre className="w-full whitespace-pre-wrap break-all bg-keyplate-3 p-6 font-mono text-sm text-keyplate-11">
+            <code>{JSON.stringify(article, null, 2)}</code>
+          </pre>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            そこでは、あらゆる事が可能である。人は一瞬にして氷雲の上に飛躍し大循環の風を従へて北に旅する事もあれば、赤い花杯の下を行く蟻と語ることもできる。
+            罪や、かなしみでさへそこでは聖くきれいにかゞやいてゐる。
+          </p>
+        </section>
+        <section className="font-article text-article">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            そこでは、あらゆる事が可能である。人は一瞬にして氷雲の上に飛躍し大循環の風を従へて北に旅する事もあれば、赤い花杯の下を行く蟻と語ることもできる。
+            罪や、かなしみでさへそこでは聖くきれいにかゞやいてゐる。
+          </p>
+        </section>
+        <section>
+          <h2 className="text-heading2 font-bold">部員一覧</h2>
+          <pre className="w-full whitespace-pre-wrap break-all bg-keyplate-3 p-6 font-mono text-sm text-keyplate-11">
+            <code>{JSON.stringify(members, null, 2)}</code>
+          </pre>
+        </section>
+      </article>
+    </div>
+  );
+};
 
 export default MembersPage;
 
