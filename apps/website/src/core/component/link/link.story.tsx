7c8a5461508df6d27c1a9e1d4af0bdce8f6320ab
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-02 03:36:27 +0900

    doc: 📝 (`<Link>`) ストーリーを修正に合わせて更新した。

--- apps/website/src/core/component/link/link.story.tsx
+++ apps/website/src/core/component/link/link.story.tsx
@@ -5,11 +5,28 @@ type Story = StoryObj<typeof Link>;
 
 const meta: Meta<typeof Link> = {
   component: Link,
-  argTypes: {},
+  args: {
+    external: undefined,
+  },
+  argTypes: {
+    external: {
+      description: 'If true, only an anchor tag <a> will be rendered instead of `next/link`.',
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
 };
 
 export default meta;
 
 export const Default: Story = {
   render: () => <Link href="/">Link</Link>,
+};
+
+export const External: Story = {
+  ...Default,
+  args: {
+    external: true,
+  },
 };
