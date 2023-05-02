e37256f8d95b8ce610d5556281063dc2da23d5b1
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-02 15:02:18 +0900

    refactor: ♻️ (website) スタイルトークンをTailwindCSSとTypescript両方から参照できるようにした。

---
+++ apps/website/src/style/token/index.ts
@@ -0,0 +1,5 @@
+import breakpoints from './breakpoint.json';
+import coreColorTokens from './color/core.json';
+
+export const colorTokens = coreColorTokens.colors;
+export const breakpointTokens = breakpoints;
