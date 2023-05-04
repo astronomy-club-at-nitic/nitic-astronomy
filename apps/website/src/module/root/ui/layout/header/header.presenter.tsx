eb556401da102319172fa092ad84340d772567f1
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-04 20:04:36 +0900

    feat: ✨ (`<Header>`) ヘッダーコンポーネントを作成した。

--- apps/website/src/module/root/ui/layout/header/header.presenter.tsx
+++ apps/website/src/module/root/ui/layout/header/header.presenter.tsx
@@ -1,29 +1,49 @@
 import type { ComponentPropsWithoutRef, FC } from 'react';
+import { Image } from '@/core/component/image';
 import { Link } from '@/core/component/link';
+import { breakpointTokens } from '@/style/token';
+import MonoDarkLogoImage from '@public/image/logo/mono-dark.png';
+import MonoLightLogoImage from '@public/image/logo/mono-light.png';
 
-type HeaderProps = ComponentPropsWithoutRef<'header'>;
+export type HeaderProps = ComponentPropsWithoutRef<'header'>;
 
-export const Header: FC<HeaderProps> = () => (
-  <header className="sticky top-0 flex w-full flex-col items-center justify-center bg-keyplate-2 p-3">
-    <hgroup className="text-center">
-      <h1 className="my-4 text-4xl font-bold">
-        <span className="rounded-full bg-primary-3 px-8 py-2 text-primary-11">Astronomy Club</span> at&nbsp;NITIC
-      </h1>
-      <p>
-        Next 13 with <code className="bg-keyplate-3 text-keyplate-11">/app</code>{' '}
-        <span className="font-bold text-teal-11">styled using Tailwind CSS</span>
-      </p>
-    </hgroup>
-    <nav className="flex flex-row flex-wrap items-center justify-center gap-4 rounded-full bg-info-3 px-4 py-2">
-      <Link className="text-info-11 underline" href="/article">
-        記事を見る
+export const Header: FC<HeaderProps> = ({ children, ...props }) => (
+  <header className="sticky top-0 flex w-full flex-col items-center justify-start bg-keyplate-1 font-bold text-keyplate-12" {...props}>
+    <nav aria-label="グローバルナビゲーション" className="flex w-full max-w-md flex-row flex-nowrap items-stretch justify-center gap-6 px-6 py-3">
+      <Link href="/" className="inline-flex shrink-0 items-center duration-100 hover:scale-110 hover:shadow-card">
+        <Image
+          src={MonoLightLogoImage}
+          alt="茨城高専天文部のロゴ"
+          sizes={`20vw, ${breakpointTokens.tablet.mediaQuery} 15vw, ${breakpointTokens.tablet} 10vw`}
+          placeholder="blur"
+          className="w-[50px] dark:hidden"
+        />
+        <Image
+          src={MonoDarkLogoImage}
+          alt="茨城高専天文部のロゴ"
+          sizes={`20vw, ${breakpointTokens.tablet.mediaQuery} 15vw, ${breakpointTokens.tablet} 10vw`}
+          placeholder="blur"
+          className="hidden w-[50px] dark:block"
+        />
       </Link>
-      <Link className="text-info-11 underline" href="/tag">
-        タグ一覧
-      </Link>
-      <Link className="text-info-11 underline" href="/member">
-        部員紹介
-      </Link>
+      <ul className="flex shrink grow flex-row items-center justify-between truncate">
+        <li className="inline-flex shrink truncate">
+          <Link href="/article" className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-primary-3 hover:text-primary-11 tablet:px-4">
+            記事を見る
+          </Link>
+        </li>
+        <li className="inline-flex shrink truncate">
+          <Link href="/tag" className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-primary-3 hover:text-primary-11 tablet:px-4">
+            タグを見る
+          </Link>
+        </li>
+        <li className="inline-flex shrink truncate">
+          <Link href="/member" className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-primary-3 hover:text-primary-11 tablet:px-4">
+            部員紹介
+          </Link>
+        </li>
+      </ul>
     </nav>
+    {children}
   </header>
 );
