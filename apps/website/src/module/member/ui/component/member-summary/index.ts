62aad3ec73215ce6c4323282358157279c561be5
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-05 20:38:17 +0900

    feat: ✨ (`<MemberSummary>`) 部員の概要表示用のコンポーネントを作成した。

---
+++ apps/website/src/module/member/ui/component/member-summary/index.ts
@@ -0,0 +1,2 @@
+export { MemberSummary } from './member-summary.presenter';
+export type { MemberSummaryProps } from './member-summary.presenter';
---
+++ apps/website/src/module/member/ui/component/member-summary/member-summary.presenter.tsx
@@ -0,0 +1,24 @@
+import type { Member } from '@nitic-astronomy/model';
+import type { FC, ComponentPropsWithoutRef } from 'react';
+import { twMerge } from '@/core/util/tw-merge.util';
+import { MemberIcon } from '@/module/member/ui/component/member-icon';
+import SquareLightLogoImage from '@public/image/logo/square-light.png';
+
+export type MemberSummaryProps = ComponentPropsWithoutRef<'li'> &
+  Pick<Member, 'name' | 'icon' | 'role'> & {
+    count?: number;
+  };
+
+export const MemberSummary: FC<MemberSummaryProps> = ({ name, icon, role, count, className, ...props }) => (
+  <li className={twMerge('flex flex-row items-center gap-2.5', className)} {...props}>
+    {icon ? (
+      <MemberIcon src={icon.url} width={icon.width ?? 64} height={icon.height ?? 64} alt={`${name} のアイコン`} />
+    ) : (
+      <MemberIcon src={SquareLightLogoImage} placeholder="blur" alt={'デフォルトの部員アイコン'} />
+    )}
+    <div className="flex flex-col items-start justify-center gap-0">
+      <p>{name}</p>
+      {role && <p className="text-small text-keyplate-11">{role}</p>}
+    </div>
+  </li>
+);
