2568e3848b3a2fb88ce7cee6b39aecbdaffcafc1
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-05 20:38:42 +0900

    doc: 📝 (`<MemberSummary>`) 部員の概要表示用のコンポーネントのストーリーを作成した。

---
+++ apps/website/src/module/member/ui/component/member-summary/member-summary.story.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MemberSummary } from './member-summary.presenter';
+
+type Story = StoryObj<typeof MemberSummary>;
+
+const meta: Meta<typeof MemberSummary> = {
+  component: MemberSummary,
+  args: {
+    name: '鈴木 結衣',
+    role: '会計・5年情報系',
+  },
+  decorators: [(Story) => <ul>{Story()}</ul>],
+  argTypes: {
+    name: {
+      description: 'A name of the member.',
+      control: {
+        type: 'text',
+      },
+    },
+    role: {
+      description: 'A displayed role of the member.',
+      control: {
+        type: 'text',
+      },
+    },
+  },
+};
+
+export default meta;
+
+export const Default: Story = {};
+
+export const WithExternalIcon: Story = {
+  args: {
+    icon: {
+      url: 'https://avatars.githubusercontent.com/u/16751535?v=4',
+    },
+    name: '白田 連大',
+    role: '会計・4年情報系',
+  },
+};
