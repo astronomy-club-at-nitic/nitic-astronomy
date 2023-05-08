0cf12603ae70c814e75629470aa00c9212c7b89c
れおお <16751535+ReoHakase@users.noreply.github.com>
2023-05-08 14:18:21 +0900

    Update apps/website/src/core/component/page-summary/page-summary.story.tsx
    Co-authored-by: shio <85730998+dino3616@users.noreply.github.com>

--- apps/website/src/core/component/page-summary/page-summary.story.tsx
+++ apps/website/src/core/component/page-summary/page-summary.story.tsx
@@ -6,11 +6,7 @@ type Story = StoryObj<typeof PageSummary>;
 const meta = {
   component: PageSummary,
   args: {
-    children: (
-      <>
-        <PageSummaryHeading>4月30日(日)の19時から茨城高専1号館屋上にて開催予定です！</PageSummaryHeading>
-      </>
-    ),
+    children: <PageSummaryHeading>4月30日(日)の19時から茨城高専1号館屋上にて開催予定です！</PageSummaryHeading>,
   },
   argTypes: {},
 } satisfies Meta<typeof PageSummary>;
